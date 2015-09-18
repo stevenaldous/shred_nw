47ea7b4976391dadf5a7403be7f25fb1446140f0
stevenaldous <stevenaldous@gmail.com>
2015-09-18 10:07:40 -0700

    mobile scaling

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,4 @@
 module.exports.connections = {
 
-  // localDiskDb: {
-  //   adapter: 'sails-disk'
-  // },
-  // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
-  //   database: 'shred_wa'
-  // }
+
 };
